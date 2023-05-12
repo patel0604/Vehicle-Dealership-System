a9654aa91925ef634871d65697bf7caaf3a01a33
Arman <a1852754@adelaide.edu.au>
2023-05-12 14:50:30 +0930

    Dealership.h

--- Dealership.h
+++ Dealership.h
@@ -6,7 +6,7 @@ using namespace std;
 
 class Dealership {
     private:
-        int capacity;
+        const int capacity;
         int num_vehicles;
         string dealership_name;
         double funds;
@@ -14,6 +14,7 @@ class Dealership {
         int parking_width;
         // pointer to a pointer of a vector of vehicles
     public:
+        //constructor for dealership
         Dealership(string dealership_name, int capacity, double funds, int parking_length, int parking_width){
             this->dealership_name = dealership_name;
             this->capacity = capacity;
@@ -23,6 +24,32 @@ class Dealership {
             num_vehicles = 0;
             // initialise the pointer to pointer
         }
+
+        //all get functions
+        int get_capacity() {return capacity;}
+        int get_num_vehicles() {return num_vehicles;}
+        string get_dealership_name() {return dealership_name;}
+        double get_funds() {return funds;}
+        int get_parking_length() {return parking_length;}
+        int get_parking_width() {return parking_width;}
+
+        //all set functions
+        string set_dealership_name(dealership_name) {this->dealership_name = dealership_name;}
+
+        //other functions
+        void change_funds(double funds_added) {funds += funds_added;}
+
+        void add_vehicle(Vehicle vehicle) {}
+
+        void remove_vehicle(Vehicle vehicle) {}
+
+        void search_vehicle(string make, string model) {}
+
+        void display_cars() {}
+
+        void display_trucks() {}
+
+        void display_vans() {}
 };
 
 #endif
