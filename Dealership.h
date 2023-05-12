d3c22371dfa0275f7515d1a4afd3110ac0b3cad4
Arman <a1852754@adelaide.edu.au>
2023-05-12 14:13:50 +0930

    Dealership.h

--- Dealership.h
+++ Dealership.h
@@ -1,14 +1,28 @@
 #ifndef DEALERSHIP_H
 #define DEALERSHIP_H
+#include <string>
+#include <iostream>
 using namespace std;
 
 class Dealership {
     private:
-
+        int capacity;
+        int num_vehicles;
+        string dealership_name;
+        double funds;
+        int parking_length;
+        int parking_width;
+        // pointer to a pointer of a vector of vehicles
     public:
-    
-}
-
-
+        Dealership(string dealership_name, int capacity, double funds, int parking_length, int parking_width){
+            this->dealership_name = dealership_name;
+            this->capacity = capacity;
+            this->funds = funds;
+            this->parking_length = parking_length;
+            this->parking_width = parking_width;
+            num_vehicles = 0;
+            // initialise the pointer to pointer
+        }
+};
 
 #endif
