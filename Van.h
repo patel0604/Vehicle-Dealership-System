efdb447c3932527cb1e6149af6dbeec14af64dc2
Arman <a1852754@adelaide.edu.au>
2023-05-13 13:00:17 +0930

    van

--- Van.h
+++ Van.h
@@ -13,9 +13,27 @@ class Van : public Truck {
         int passenger_limit;
     public:
         //passenger
-        Van(int length, int width, double weight, double cost, string brand, string model, string number_plate, int load_capacity, int passenger_limit) : Truck(length, width, weight, cost, brand, model, number_plate, load_capacity) {
-            this->
+        Van(int length, int width, double weight, double cost, string brand, string model, string number_plate, int load_capacity, int passenger_limit) {
+            this->length = length;
+            this->width = width;
+            this->cost = cost;
+            this->brand = brand;
+            this->model = model;
+            this->number_plate = number_plate;
+            this->load_capacity = load_capacity;
+            this->passenger_limit = passenger_limit;
+            van_type = "passenger";
+            load_volume = 0;
         }
+
+        //transport
+        Van(int length, int width, double weight, double cost, string brand, string model, string number_plate, int load_volume, int load_capacity) : Truck(length, width, weight, cost, brand, model, number_plate, load_volume, load_capacity) {
+            van_type = "transport";
+            passenger_limit = 0;
+        }
+
+        string get_van_type() {return van_type;}
+        int get_passenger_limit() {return passenger_limit;}
 };
 
 #endif
