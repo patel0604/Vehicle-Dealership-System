27c0132866dd7ca6eb0eda947e2edd694f7ae283
koststyl <a1851357@adelaide.edu.au>
2023-05-16 12:15:30 +0930

    Truck Testing

--- main.cpp
+++ main.cpp
@@ -2,11 +2,24 @@
 #include <string>
 #include "Vehicle.h"
 #include "Van.h"
+#include "Car.h"
+#include "Truck.h"
 using namespace std;
 
 int main() {
     Van van1(250,150,1500,15000,"toyota","tvan","SW390",1000,500,7);
     Vehicle* vehicle1 = &van1;
     cout << vehicle1->get_model() << endl;
+    
+
+    Truck truck1; /*testing def. contructor*/
+    Vehicle* vehicle2 = &truck1;
+    cout << vehicle2 -> get_length() << endl;
+    cout << vehicle2 -> get_load_capacity() << endl;
+
+    Truck truck2(700, 300, 5000, 80000, "Scania", "L-Series", "S123ABC", 30000, 12345); /*testing std. contructor*/
+    Vehicle* vehicle3 = &truck2;
+    cout << vehicle3 -> get_brand() << endl;
+
     return 0;
 }
