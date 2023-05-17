8bc7a74df5c428b7db17cbfe2f348f559274eb8a
Arman <a1852754@adelaide.edu.au>
2023-05-17 19:10:01 +0930

    seller

--- Seller.h
+++ Seller.h
@@ -15,38 +15,97 @@ class Seller : public Customer {
         //seller constructor
         Seller(string name, string licence_number, double wallet, int vehicle_amount) : Customer(name, licence_number, wallet, vehicle_amount) {}
 
-        Vehicle initialise_vehicle() {
-            for (int i = 0; i < vehicle_amount; i++) {
-                cout << "what type of vehicle would you like to initialise?" << endl;
+        //function for initialising the vehicles in the array to be sold
+        void initialise_vehicles() {
+            //if there is enough space in the array the for loop commences
+            for (vehicles_index; vehicles_index < vehicle_amount; vehicles_index++) {
+                cout << "enter 'car', 'truck' or 'van' for the type of vehicle you want to initialise" << endl;
                 string vehicle_type;
                 cin >> vehicle_type;
-                swith (vehicle_type) {
-                    case "car":
-                        cout << "enter the brand: " << endl;
-                        string brand;
-                        cin >> brand;
-                        cout << "enter the model: " << endl;
-                        string model;
-                        cin >> model;
-                        cout << "enter the licence plate: " << endl;
-                        string licence_plate;
-                        
-                        break;
-                    case "truck":
-                        //if truck
-                        break;
-                    case "van":
-                        //if van
-                        break;
-                    default:
-                        cout << "entered wrong vehicle type, function aborted" << endl;
-                        cout << "the vehicle type must either be 'car', 'truck, or 'van'" << endl;
-                }
+                //ask basic values
+                cout << "enter the brand: " << endl;
+                string brand;
+                cin >> brand;
+                cout << "enter the model: " << endl;
+                string model;
+                cin >> model;
+                cout << "enter the number plate: " << endl;
+                string number_plate;
+                cin >> number_plate;
+                cout << "enter the length in cm: " << endl;
+                int length;
+                cin >> length;
+                cout << "enter the width in cm: " << endl;
+                int width;
+                cin >> width;
+                cout << "enter the weight in kg: " << endl;
+                double weight;
+                cin >> weight;
+                cout << "enter the cost in $: " << endl;
+                double cost;
+                cin >> cost;
+
+                if (vehicle_type == "car") {
+                    //initialise a new car with the given values
+                    Car car(length, width, weight, cost, brand, model, number_plate);
+                    //copy the contents of the object to a vehicle in the seller array
+                    *vehicles[vehicles_index] = car;
+                    //when the if statement ends, the object will delete itself
+                } else if (vehicle_type == "truck") {
+                    cout << "enter the load volume in L: " << endl;
+                    int load_volume;
+                    cin >> load_volume;
+                    cout << "enter the load capacity in kg: " << endl;
+                    int load_capacity;
+                    cin >> load_capacity;
+                    //initialise a new truck with the given values
+                    Truck truck(length, width, weight, cost, brand, model, number_plate, load_volume, load_capacity);
+                    //copy the contents of the object to a vehicle in the seller array
+                    *vehicles[vehicles_index] = truck;
+                } else if (vehicle_type == "van") {
+                    string van_type;
+                    cout << "enter 'passenger' or 'transport' for the van type you want to initialise: " << endl;
+                    cin >> van_type;
+                    if (van_type == "passenger") {
+                        int load_volume = 0;
+                        cout << "enter the load capacity of passengers in kg: " << endl;
+                        int load_capacity;
+                        cin >> load_capacity;
+                        cout << "enter the number of passenger limit: " << endl;
+                        int passenger_limit;
+                        cin >> passenger_limit;
+                        //initialise a new van with the given values
+                        Van van(length, width, weight, cost, brand, model, number_plate, load_volume, load_capacity, passenger_limit);
+                        //copy the contents of the object to a vehicle in the seller array
+                        *vehicles[vehicles_index] = van;
+                    } else if (van_type == "transport") {
+                        cout << "enter the load volume in L: " << endl;
+                        int load_volume;
+                        cin >> load_volume;
+                        cout << "enter the load capacity of cargo in kg: " << endl;
+                        int load_capacity;
+                        cin >> load_capacity;
+                        int passenger_limit = 0;
+                        //initialise a new van with the given values
+                        Van van(length, width, weight, cost, brand, model, number_plate, load_volume, load_capacity, passenger_limit);
+                        //copy the contents of the object to a vehicle in the seller array
+                        *vehicles[vehicles_index] = van;
+                    } else {
+                        cout << "wrong van type entered" << endl;
+                    }
+                } else {
+                    cout << "entered wrong vehicle type entered" << endl;
+                }  
             }
         }
 
-        void vehicle_transaction() { //also add a cost conditional
-            //remove a vehicle from the vector and add it to the dealership
+        //remove a vehicle from the vector and add it to the dealership
+        void vehicle_transaction(Vehicle* vehicle) { //also add a cost conditional
+            //check if there are any vehicles in the array
+            if (vehicles_index > 0) {
+                cout << "there are seller vehicles to be sold" << endl;
+                
+            }
         }
 
         ~Seller() {
