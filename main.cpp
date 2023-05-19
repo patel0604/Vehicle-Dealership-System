1cf54321d370d9dc93e14e449ec9f77b5d59efb8
koststyl <a1851357@adelaide.edu.au>
2023-05-19 14:24:13 +0930

    Changes to main for interface

--- main.cpp
+++ main.cpp
@@ -8,7 +8,28 @@ using namespace std;
 
 int main() {
 
-    /* Van testing*/
+    // Now writing code for main menu (user interface)
+    int user_selection1;
+    
+    cout << "Welcome to the dealership!" << endl;
+    cout << "Enter the number corresponding to which action you want to do." << endl;
+    cout << "1. Adding (buying) a car to the dealership." << endl;
+    cin >> user_selection1;
+
+    if (user_selection1 == 1){
+        // run code for adding a car to the dealership
+    } 
+
+    while (user_selection1 != 1){
+    // if user enters a number not shown 
+        cout << "Please enter a number shown." << endl;
+        cin >> user_selection1;
+    }
+    
+
+    /*
+
+    // Van testing
     Van van1(250,150,1500,15000,"toyota","tvan","SW390",1000,500,7);
     Vehicle* vehicle1 = &van1;
     cout << vehicle1->get_model() << endl;
@@ -17,23 +38,29 @@ int main() {
     Vehicle* vehicle2 = &car1;
     cout << vehicle2->get_cost() << endl;
     
-    /*Truck testing*/
-    Truck truck1; /*testing def. contructor*/
+    //Truck testing
+    Truck truck1; //testing def. contructor
     Vehicle* vehicle3 = &truck1;
     cout << vehicle3 -> get_length() << endl;
     cout << vehicle3 -> get_load_capacity() << endl;
 
-    Truck truck2(700, 300, 5000, 80000, " ", "L-Series", "S123ABC", 30000, 12345); /*testing std. contructor*/
+    Truck truck2(700, 300, 5000, 80000, " ", "L-Series", "S123ABC", 30000, 12345); //testing std. contructor
     Vehicle* vehicle4 = &truck2;
     cout << vehicle4 -> get_brand() << endl;
 
-    /*Vehicle testing*/
-    Vehicle vehicle_a; /*tesing def. constructor*/
+    //Vehicle testing
+    Vehicle vehicle_a; //tesing def. constructor
     cout << vehicle_a.get_brand() << endl;
 
-    Vehicle vehicle_b(1,2,3,4,5); /*testing for incorrect amount of arguments for the contructor*/
+    Vehicle vehicle_b(1,2,3,4); //testing for incorrect amount of arguments for the contructor
+
+    */
 
 
+
+
+
+    
 
     return 0;
 }
