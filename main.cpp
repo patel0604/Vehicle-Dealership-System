0ca27bc3a9627de6131840fb1e8eb3814e03c38f
koststyl <a1851357@adelaide.edu.au>
2023-05-21 19:04:58 +0930

    UI updates - still require more functions for user inputs

--- main.cpp
+++ main.cpp
@@ -36,34 +36,34 @@ void UI::mainMenu(){
     cout << "1. Adding (buying) a car to the dealership." << endl;
     cout << "2. Removing (selling) a car from the dealership." << endl;
     cout << "3. Check dealership"<<endl;
-    cout << "0. Close the program." << endl;
+    cout << "4. Close the program." << endl;
     cout << " "<<endl;
     cout << "Enter the number corresponding to which action you want to do." << endl;
     cin >> user_choice1;
     cout << endl; // leave this here for a gap between menu and next section
 
-    //stage 1
+    //stage 1.1
     //Checking for the correct input type
     //user must input a number shown
 
-    while(user_choice1 != "0" &&
-                user_choice1 != "1" &&
+    while(user_choice1 != "1" &&
                 user_choice1 != "2" &&
-                user_choice1 != "3"){
+                user_choice1 != "3" &&
+                user_choice1 != "4"){
         cout << "Please enter a number shown in the main menu." << endl;
         cin >> user_choice1;
     }
 
-    //stage 2
+    //stage 1.2
 
     if (user_choice1 == "1"){
-        cout << "Adding (buying) a car to the dealership." << endl;
+        cout << "You selected: Adding (buying) a car to the dealership." << endl;
     } else if (user_choice1 == "2"){
-        cout << "Removing (selling) a car from the dealership." << endl;
+        cout << "You selected: Removing (selling) a car from the dealership." << endl;
     } else if (user_choice1 == "3"){
         cout <<"Checking the dealership." << endl; 
         checkDealership();
-    } else if (user_choice1 == "0"){
+    } else if (user_choice1 == "4"){
         cout << "Closing the program." << endl;
     } 
     
@@ -73,7 +73,7 @@ void UI::mainMenu(){
     
 void UI::checkDealership() {
 
-    int user_choice2;
+    std::string user_choice2;
 
     cout << "*****************************************************\n";
     cout << "               Dealership inventory menu             \n";
@@ -84,80 +84,60 @@ void UI::checkDealership() {
     cout << "          4. Back to main menu  \n";
     cout << "          Please select one of the options above   ";
 
-    cin>>user_choice2;
-    switch (user_choice2){
-        case 1:
-        {
-            checkVehicles();
-            break;
-        }
-
-        case 2:
-        {
-            cout<<"test"<<endl;
-            break;
-
-        }
-        case 3:
-        {
-            cout<<"test"<<endl; 
-            break;           
-        }
-
-        case 4:
-        {
-            mainMenu();
-            break;
-        }
-
-        default:
-        {
-            cout<<"Please choose from one of the options"<<endl;
-        }
+    //checking for correct input
+    while(user_choice2 != "1" &&
+                user_choice2 != "2" &&
+                user_choice2 != "3" &&
+                user_choice2 != "4"){
+        cout << "Please enter a number shown in the Dealership Inventory menu above." << endl;
+        cin >> user_choice2;
     }
+
+    if (user_choice2 == "1"){
+        cout << "You selected: Check vehicles." << endl;
+        checkVehicles();
+    } else if (user_choice2 == "2"){
+        cout << "You selected: Check funds." << endl;
+    } else if (user_choice2 == "3"){
+        cout <<"You selected: Deposit funds." << endl; 
+    } else if (user_choice2 == "4"){
+        cout << "Returning to main menu." << endl;
+        mainMenu();
+    } 
+    
 }
+
 
 void UI::checkVehicles(){
 
-    int user_choice3;
+    std::string user_choice3;
     cout << "*****************************************************\n";
-    cout << "                Vehicle's menu             \n";
+    cout << "                Vehicles menu             \n";
     cout << "*****************************************************\n";
     cout << "          1. List all vehicles  \n";
     cout << "          2. Edit vehicle details  \n";
     cout << "          3. Back to main menu  \n";
     cout << "          Please select one of the options above   ";
 
-    cin>>user_choice3;
-    switch (user_choice3){
-        case 1:
-        {
-            cout<<"test"<<endl;
-            break;
-        }
-
-        case 2:
-        {
-            cout<<"test"<<endl;
-            break;
-        }
-        case 3:
-        {
-            mainMenu();
-            break;
-        }
-
-        default:
-        {
-            while (user_choice3 != 1
-                || user_choice3 != 2
-                || user_choice3 != 3)
-            cout<<"Please choose from one of the options"<<endl;
-        }
-
+    //checking for correct input
+    while(  user_choice3 != "1" &&
+            user_choice3 != "2" &&
+            user_choice3 != "3"){
+        cout << "Please enter a number shown in the Vehicles menu above." << endl;
+        cin >> user_choice3;
     }
-}
 
+    //user's options
+    if (user_choice3 == "1"){
+        cout << "You selected: List all vehicles." << endl;
+    } else if (user_choice3 == "2"){
+        cout << "You selected: Edit Vehicle details." << endl;
+    } else if (user_choice3 == "3"){
+        cout << "Returning to main menu." << endl;
+        mainMenu();
+    } 
+    
+}
 
 
 int main(){
