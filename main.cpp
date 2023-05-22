df38d5903d9dd33651108ebc2d5aff29107e8820
Hard Patel <hardpatel@student-10-201-03-192.wireless.adelaide.edu.au>
2023-05-22 11:42:03 +0930

    main file UI replaced

--- main.cpp
+++ main.cpp
@@ -8,8 +8,18 @@
 #include "Truck.h"
 using namespace std;
 
-int main() {
-    // Now writing code for main menu (user interface)
+class UI{
+    private:
+        void mainMenu();
+        void checkDealership();
+        void checkVehicles();
+
+    public:
+        void runProgram();
+
+};
+
+void UI::mainMenu(){
     int user_choice1;
 
     cout << "*****************************************************\n";
@@ -19,13 +29,13 @@ int main() {
     cout << "            Welcome to the Dealership!        " << endl;
     cout << "1. Adding (buying) a car to the dealership." << endl;
     cout << "2. Removing (selling) a car from the dealership." << endl;
-    cout << "3. Check dealership";
-
+    cout << "3. Check dealership"<<endl;
     cout << "0. Close the program." << endl;
+    cout << " "<<endl;
     cout << "Enter the number corresponding to which action you want to do." << endl;
     cin >> user_choice1;
     cout << endl; // leave this here for a gap between menu and next section
-    
+
     //stage 1
     //Entering the correct input type
     //user must input a number shown
@@ -33,12 +43,21 @@ int main() {
     switch (user_choice1){
         case 1:
         {
-            //add_vehicle();
+            cout<<"test"<<endl;
+            break;
+
         }
 
         case 2:
         {
+            cout<<"test"<<endl;
+            break;
+        }
 
+        case 3:
+        {
+            checkDealership();
+            break;
         }
         case 0:
         {
@@ -49,123 +68,102 @@ int main() {
         {
             cout<<"Please choose from one of the options"<<endl;
         }
-    
+
     }
+
+}
     
-    void checkdealership(); {
+void UI::checkDealership() {
 
-        int user_choice2;
+    int user_choice2;
 
-        cout << "*****************************************************\n";
-        cout << "               Dealership inventory menu             \n";
-        cout << "*****************************************************\n";
-        cout << "          1. Amount of vehicles  \n";
-        cout << "          2. Check funds  \n";
-        cout << "          3. Deposit Funds  \n";
-        cout << "          4. Back to main menu  \n";
-        cout << "          Please select one of the options above   ";
+    cout << "*****************************************************\n";
+    cout << "               Dealership inventory menu             \n";
+    cout << "*****************************************************\n";
+    cout << "          1. Check Vehicles  \n";
+    cout << "          2. Check funds  \n";
+    cout << "          3. Deposit Funds  \n";
+    cout << "          4. Back to main menu  \n";
+    cout << "          Please select one of the options above   ";
 
-        cin>>user_choice2;
-        switch (user_choice2){
+    cin>>user_choice2;
+    switch (user_choice2){
         case 1:
         {
-            
+            cout<<"test"<<endl;
+            break;
         }
 
         case 2:
         {
+            cout<<"test"<<endl;
+            break;
 
         }
         case 3:
         {
-            
+            cout<<"test"<<endl; 
+            break;           
         }
 
         case 4:
         {
-
+            mainMenu();
+            break;
         }
 
         default:
         {
             cout<<"Please choose from one of the options"<<endl;
         }
-    
     }
+}
 
+void UI::checkVehicles(){
 
+    int user_choice3;
+    cout << "*****************************************************\n";
+    cout << "                Vehicle's menu             \n";
+    cout << "*****************************************************\n";
+    cout << "          1. List all vehicles  \n";
+    cout << "          2. Edit vehicle details  \n";
+    cout << "          3. Back to main menu  \n";
+    cout << "          Please select one of the options above   ";
+
+    cin>>user_choice3;
+    switch (user_choice3){
+        case 1:
+        {
+            cout<<"test"<<endl;
+            break;
+        }
+
+        case 2:
+        {
+            cout<<"test"<<endl;
+            break;
+        }
+        case 3:
+        {
+            mainMenu();
+            break;
+        }
+
+        default:
+        {
+            cout<<"Please choose from one of the options"<<endl;
+        }
 
     }
-    
-    
-    /*// if user enters a number not shown 
-    while (user_choice1 != "1"
-        && user_choice1 != "2"
+}
 
-        && user_choice1 != "0"){
+void UI::runProgram(){
+    mainMenu();
+}
 
-        cout << "Please enter a number shown in the menu." << endl;
-        cin >> user_choice1;
-    }
-
-    //stage 2
-    //going down the path of the correct function
-    //cannot use a switch statement as they are only for int types
-
-    std::string user_choice2;
-
-    if (user_choice1 == "1"){
-        cout << "You selected 1. Adding (buying) a car to the dealership." << endl;
-        cout << << endl;
-    }
-
-    if (user_choice1 == "2"){
-        cout << "You selected 2. Removing (selling) a car from the dealership." << endl;
-    }
-
-    if (user_choice1 == "0"){
-        cout << "You selected 0. Closing the program." << endl;
-    }
-
-
-
-    
-    
-
-    /*
-
-    // Van testing
-    Van van1(250,150,1500,15000,"toyota","tvan","SW390",1000,500,7);
-    Vehicle* vehicle1 = &van1;
-    cout << vehicle1->get_model() << endl;
-
-    Car car1(30,150,1500, -15000,"","camry","KS390");
-    Vehicle* vehicle2 = &car1;
-    cout << vehicle2->get_cost() << endl;
-    
-    //Truck testing
-    Truck truck1; //testing def. contructor
-    Vehicle* vehicle3 = &truck1;
-    cout << vehicle3 -> get_length() << endl;
-    cout << vehicle3 -> get_load_capacity() << endl;
-
-    Truck truck2(700, 300, 5000, 80000, " ", "L-Series", "S123ABC", 30000, 12345); //testing std. contructor
-    Vehicle* vehicle4 = &truck2;
-    cout << vehicle4 -> get_brand() << endl;
-
-    //Vehicle testing
-    Vehicle vehicle_a; //tesing def. constructor
-    cout << vehicle_a.get_brand() << endl;
-
-    Vehicle vehicle_b(1,2,3,4); //testing for incorrect amount of arguments for the contructor
-
-    */
-
-
-
-
-
-    
+int main(){
+    UI ui;
+    ui.runProgram();
 
     return 0;
 }
