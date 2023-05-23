55bef3e8d7f7040e887a53575fdb3a6b18333b3f
Hard Patel <hardpatel@student-10-201-03-060.wireless.adelaide.edu.au>
2023-05-23 12:41:20 +0930

    mainupdate

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 #include <string>
 #include <limits>
-//#include "Dealership.h"
 #include "Vehicle.h"
 #include "Van.h"
 #include "Car.h"
 #include "Truck.h"
+#include "Dealership.h"
 using namespace std;
 
 class UI{
@@ -232,6 +232,10 @@ void UI::dealershipMenu() {
 }
 
 int main(){
+    Dealership dealership1;
+
+    
+
     UI ui;
     ui.runProgram();
 
