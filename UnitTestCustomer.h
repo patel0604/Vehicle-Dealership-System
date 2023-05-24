8297b90e08dfa2b615c40271c884291251a28eb2
koststyl <a1851357@adelaide.edu.au>
2023-05-24 14:59:10 +0930

    Unit testing

--- UnitTestCustomer.h
+++ UnitTestCustomer.h
@@ -21,12 +21,20 @@ class UnitTestCustomer {
             //initialise objects
             Dealership dealership("Auto shop", 3, 50000, 300, 200);
 
+            Seller seller1("Judas White", "RTN9E20", 10000, 4);
+
+            //
+            Seller seller2("Walter", "FV1234", 100000, 5);
+
+
             //
             // dealership doesn't have enough money
-            // note the cost is $500000
+            // note the cost is $500000 and dealership has $50000
             Vehicle* car2 = new Car(250, 150, 1300, 500000, "Bugatti", "Tate's Bugatti", "WCIYB16");
-            dealership.add_vehicle(car2);
+            seller1.vehicle_transaction("WCIYB16", &dealership);
+            seller1.display_vehicles();
             dealership.display_cars();
+
 
             //
             // vehicle dimensions not met in dealership from a seller's vehicle
@@ -39,7 +47,7 @@ class UnitTestCustomer {
             Vehicle* truck1 = new Truck(300, 200, 2000, 25000, "Volvo", "VNL 860", "UNR389", 1000, 500);
             Vehicle* van1 = new Van(270, 175, 1400, 15000, "Mercedes-Benz", "Sprinter", "KLE38X2", 0, 400, 7);
             Buyer buyer1("John Doe", "JREO92", 55000, 3);
-            Seller seller1("Judas White", "RTN9E20", 10000, 4);
+            
 
             //
             // testing for selling vehicles in different order
@@ -110,14 +118,17 @@ class UnitTestCustomer {
             cout << "seller wallet is: " << seller1.get_wallet() << endl;
 
             //display seller vehicles
-            seller1.display_vehicles();
-
-            
+            seller1.display_vehicles();            
 
 
 
             // sell more vehicles than in dealership
             
+
+
+            
+            
+
 
         }
 };
