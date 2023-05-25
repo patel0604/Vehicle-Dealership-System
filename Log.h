a9059a4ac645c8ba3a99790d90462e7103fbde8b
koststyl <a1851357@adelaide.edu.au>
2023-05-25 15:51:26 +0930

    .cpp defintions

--- Log.h
+++ Log.h
@@ -11,44 +11,20 @@ using namespace std;
 class Log {
     public:
         //initialises a vehicle from a file into a given pointer
-        void write(string file_name, Dealership dealership) {
-            //
-            ofstream write_dealership(file_name); //open the dealership file
-            if (write_dealership.is_open()) { //check if the file was opened
-                write_dealership << "19/05/2023" << endl;
-                write_dealership << "vehicle number 1 is car" << endl;
-                
-                write_dealership.close(); //close the file
-                cout << "written successfully" << endl;
-                
-            } else {
-                cout << "failed to open the file" << endl;
-            }
-        }
+        void write(string file_name, Dealership dealership);
 
-        void read(string file_name) {
-            ifstream read_dealership(file_name);
-            if (read_dealership.is_open()) {
-                string line;
-                while (getline(read_dealership, line)) {
-                    cout << line << endl; //print each line to the terminal
-                }
-                read_dealership.close(); //close the file
-            } else {
-                cout << "failed to open the file" << endl;
-            }
-        }
+        void read(string file_name);
 };
 
 #endif
 
-                            if (vehicle->get_vehicle_type() == "car") {
-                                dealership[i] = new Car;
-                            } else if (vehicle->get_vehicle_type() == "truck") {
-                                dealership[i] = new Truck;
-                            } else if (vehicle->get_vehicle_type() == "Van") {
-                                dealership[i] = new Van;
-                            } else {
-                                cout << "invalid vehicle type" << endl;
-                                return false;
-                            }
+if (vehicle->get_vehicle_type() == "car") {
+    dealership[i] = new Car;
+} else if (vehicle->get_vehicle_type() == "truck") {
+    dealership[i] = new Truck;
+} else if (vehicle->get_vehicle_type() == "Van") {
+    dealership[i] = new Van;
+} else {
+    cout << "invalid vehicle type" << endl;
+    return false;
+}
