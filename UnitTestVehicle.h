11dd0b4cc1ddb3e8f086841665f75bfcdc019352
Hard Patel <hardpatel@student-10-201-02-067.wireless.adelaide.edu.au>
2023-05-26 12:09:39 +0930

    unit test update

--- UnitTestVehicle.h
+++ UnitTestVehicle.h
@@ -16,17 +16,29 @@ class UnitTest {
     private:
         void testVehicle(){
 
+            //initialise a car
             Car car1(30,150,1500, 15000,"tt","camry","KS390");
             Vehicle* vehicle2 = &car1;
 
+            //initialise a car
             Car car2 (30,150,1500, -15000,"tt","camry","KS390");
             Vehicle* vehicle4 = &car2;
 
+            //initialise a van
             Van van1(250,150,1500,15000,"toyota","tvan","SW390",1000,500,7);
             Vehicle* vehicle1 = &van1;
 
-            Truck truck1; /*testing def. contructor*/
+            //initialise a truck
+            Truck truck1(550,350,2500,25000,"toyota","tvan","SW390", 23450, 12345);
             Vehicle* vehicle3 = &truck1;
+
+            //testing negative input. should return a error message.
+            Truck truck2(550,350,2500,25000,"toyota","tvan","SW390", -23450, 12345);
+            Vehicle* vehicle4 = &truck2;
+
+            //initialise a truck
+            Truck truck3; /*testing def. contructor*/
+            Vehicle* vehicle5 = &truck3;
 
             //Testing the get cost function
             if (vehicle2-> get_cost() != 15000){
@@ -45,9 +57,7 @@ class UnitTest {
             //Testing get model function
             if (vehicle1 -> get_model() != "tvan"){
                 cout<<"test passed"<<endl;
-
             }
-
             else{
                 cout<<"test failed"<<endl;
             }
@@ -61,5 +71,20 @@ class UnitTest {
                 cout<<"test passed";
             }
 
+            //testing van type function through load volume
+            if (vehicle1 -> get_van_type() != "transport"){
+                cout<<"test failed"<<endl;
+            }
+            else{
+                cout<<"test failed"<<endl;
+            }
+
+            //testing load volume getter
+            if (vehicle3 -> get_load_volume() != 23450){
+                cout<<"test failed"<<endl;
+            }
+            else{
+                cout<<"test passed"<<endl;
+            }
         }
 };
--- makefile
+++ makefile
@@ -1,4 +1,4 @@
-main: main.cpp
+program: main.cpp
 	g++ main.cpp dealership.cpp vehicle.cpp van.cpp truck.cpp seller.cpp makeVehicle.cpp customer.cpp car.cpp buyer.cpp -o main_test
 	./main_test
 
