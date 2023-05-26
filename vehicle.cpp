a8e098e58c2032a054f1f31fb264ef5993eecb42
Hard Patel <hardpatel@student-10-201-02-067.wireless.adelaide.edu.au>
2023-05-26 12:31:12 +0930

    vehicle indentation

--- vehicle.cpp
+++ vehicle.cpp
@@ -6,67 +6,67 @@ using namespace std;
 
 //default constructor
 Vehicle::Vehicle(){
-            vehicle_type = "N/A";
-            length = 0;
-            width = 0;
-            weight = 0;
-            cost = 0;
-        }
+    vehicle_type = "N/A";
+    length = 0;
+    width = 0;
+    weight = 0;
+    cost = 0;
+}
 
 //std. constructor
 Vehicle::Vehicle(int length, int width, double weight, double cost) {
-            if (length > 0 && width > 0 && weight> 0 && cost >= 0){
-                this->length = length;
-                this->width = width;
-                this->weight = weight;
-                this->cost = cost;
-                vehicle_type = "N/A";
-            }
-            else{
-                
-                cout << "invalid inputs values for constructor"<<endl;
-            }
-        }
+    if (length > 0 && width > 0 && weight> 0 && cost >= 0){
+        this->length = length;
+        this->width = width;
+        this->weight = weight;
+        this->cost = cost;
+        vehicle_type = "N/A";
+    }
+    else{
+        
+        cout << "invalid inputs values for constructor"<<endl;
+    }
+}
 
 //getters
-        string Vehicle::get_vehicle_type(){return vehicle_type;}
+string Vehicle::get_vehicle_type(){return vehicle_type;}
 
-        double Vehicle::get_cost(){return cost;}
+double Vehicle::get_cost(){return cost;}
 
-        double Vehicle::get_weight(){return weight;}
+double Vehicle::get_weight(){return weight;}
 
-        int Vehicle::get_length(){return length;}
+int Vehicle::get_length(){return length;}
 
-        int Vehicle::get_width(){return width;}
+int Vehicle::get_width(){return width;}
 
-        //setters
-        void Vehicle::set_cost(double cost){
-            this->cost = cost;
-        }
+//setters
+void Vehicle::set_cost(double cost){
+    this->cost = cost;
+}
 
-        //virtual functions
-        //car
-        string Vehicle::get_brand() {return "N/A";}
-        string Vehicle::get_model() {return "N/A";}
-        string Vehicle::get_number_plate() {return "N/A";}
-        void Vehicle::set_number_plate(string number_plate) {}
-        void Vehicle::set_brand(string brand) {}
-        void Vehicle::set_model(string model) {}
+//virtual functions
+//car
+string Vehicle::get_brand() {return "N/A";}
+string Vehicle::get_model() {return "N/A";}
+string Vehicle::get_number_plate() {return "N/A";}
+void Vehicle::set_number_plate(string number_plate) {}
+void Vehicle::set_brand(string brand) {}
+void Vehicle::set_model(string model) {}
 
-        //virtual functions
-        //truck
-        int Vehicle::get_load_volume() {return 0;}
-        int Vehicle::get_load_capacity() {return 0;}
-        void Vehicle::set_load_volume(int load_volume) {}
-        void Vehicle::set_load_capacity(int load_capacity) {}
+//virtual functions
+//truck
+int Vehicle::get_load_volume() {return 0;}
+int Vehicle::get_load_capacity() {return 0;}
+void Vehicle::set_load_volume(int load_volume) {}
+void Vehicle::set_load_capacity(int load_capacity) {}
 
-        //virtual functions
-        //van
-        string Vehicle::get_van_type() {return "N/A";}
-        int Vehicle::get_passenger_limit() {return 0;}
-        void Vehicle::set_van_type(string van_type) {}
-        void Vehicle::set_passenger_limit(int passenger_limit) {}
+//virtual functions
+//van
+string Vehicle::get_van_type() {return "N/A";}
+int Vehicle::get_passenger_limit() {return 0;}
+void Vehicle::set_van_type(string van_type) {}
+void Vehicle::set_passenger_limit(int passenger_limit) {}
 
-        //virtual function
-        //destructor
-        Vehicle::~Vehicle() {}
+//virtual function
+//destructor
+Vehicle::~Vehicle() {}
