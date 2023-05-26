8346c28db92df21a562d478987ef2eb3b6d1e338
Hard Patel <hardpatel@student-10-201-02-067.wireless.adelaide.edu.au>
2023-05-26 12:36:27 +0930

    indent

--- van.cpp
+++ van.cpp
@@ -8,41 +8,43 @@ using namespace std;
 
 Van:: Van(int length, int width, double weight, double cost, string brand, string model, string number_plate, int load_volume, int load_capacity, int passenger_limit){
     if (length > 0 && width > 0 && weight > 0 && cost >= 0 && brand != "" && model != "" && load_capacity > 0) {
-                if (load_volume == 0 && passenger_limit != 0) {
-                    vehicle_type = "van";
-                    van_type = "passenger"; 
-                    this->length = length;
-                    this->width = width;
-                    this->weight = weight;
-                    this->cost = cost;
-                    this->brand = brand;
-                    this->model = model;
-                    this->number_plate = number_plate;
-                    this->load_volume = 0;
-                    this->load_capacity = load_capacity;
-                    this->passenger_limit = passenger_limit;
-                    cout << "initialised passenger van" << endl;
-                } else if(passenger_limit == 0 && load_volume != 0) {
-                    vehicle_type = "van";
-                    van_type = "transport";
-                    this->length = length;
-                    this->width = width;
-                    this->weight = weight;
-                    this->cost = cost;
-                    this->brand = brand;
-                    this->model = model;
-                    this->number_plate = number_plate;
-                    this->load_volume = load_volume;
-                    this->load_capacity = load_capacity;
-                    this->passenger_limit = 0;
-                    cout << "initialised transport van" << endl;
-                } else {
-                    cout << "wrong inputs for van type" << endl;
-                    van_type = "N/A";
-                }
-            } else {
-                cout << "invalid input values for constructor" << endl;
-            }
+        if (load_volume == 0 && passenger_limit != 0) {
+            vehicle_type = "van";
+            van_type = "passenger"; 
+            this->length = length;
+            this->width = width;
+            this->weight = weight;
+            this->cost = cost;
+            this->brand = brand;
+            this->model = model;
+            this->number_plate = number_plate;
+            this->load_volume = 0;
+            this->load_capacity = load_capacity;
+            this->passenger_limit = passenger_limit;
+            cout << "initialised passenger van" << endl;
+        } 
+        else if(passenger_limit == 0 && load_volume != 0) {
+            vehicle_type = "van";
+            van_type = "transport";
+            this->length = length;
+            this->width = width;
+            this->weight = weight;
+            this->cost = cost;
+            this->brand = brand;
+            this->model = model;
+            this->number_plate = number_plate;
+            this->load_volume = load_volume;
+            this->load_capacity = load_capacity;
+            this->passenger_limit = 0;
+            cout << "initialised transport van" << endl;
+        } 
+        else {
+            cout << "wrong inputs for van type" << endl;
+            van_type = "N/A";
+        }
+    } else {
+        cout << "invalid input values for constructor" << endl;
+    }
 }
 
 Van:: Van():Truck(){
